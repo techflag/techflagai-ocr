7408c5e5de560c9cc7f7d740e31f72b763bbebc2
techflag <562635045@qq.com>
2025-04-27 16:59:43 +0800

    1、修改菜单 2、实现部分页面功能

---
+++ web/src/pages/dataset_list.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="page-layout">
+    <page-header ref="pageHeader" :style="`margin-top: 0px`" >
+      <slot name="action"  slot="action"></slot>
+      <slot slot="content" name="headerContent"></slot>
+      <div slot="content" v-if="!this.$slots.headerContent && desc">
+        <p>{{desc}}</p>
+        <div v-if="this.linkList" class="link">
+          <template  v-for="(link, index) in linkList">
+            <a :key="index" :href="link.href"><a-icon :type="link.icon" />{{link.title}}</a>
+          </template>
+        </div>
+      </div>
+      <slot v-if="this.$slots.extra" slot="extra" name="extra"></slot>
+    </page-header>
+    <div class="card-list">
+      <div slot="extra">
+        <a-row>
+          <a-col :span="8">
+            <a-radio-group>
+            <a-radio-button>全部</a-radio-button>
+            <a-radio-button>进行中</a-radio-button>
+            <a-radio-button>等待中</a-radio-button>
+          </a-radio-group>
+          </a-col>
+          <a-col :span="8">
+            <a-select
+              ref="select"
+              style="width: 120px"
+            >
+              <a-select-option value="jack">Jack</a-select-option>
+              <a-select-option value="lucy">Lucy</a-select-option>
+              <a-select-option value="disabled" disabled>Disabled</a-select-option>
+              <a-select-option value="Yiminghe">yiminghe</a-select-option>
+            </a-select>
+          </a-col>
+          <a-col :span="8">
+            <a-button type="primary" html-type="submit">上传数据</a-button>
+          </a-col>
+      </a-row>
+        
+      </div>
+      
+    <a-list
+      :grid="{gutter: 24, lg: 3, md: 2, sm: 1, xs: 1}"
+      :dataSource="dataSource"
+      :pagination="{showSizeChanger: true, showQuickJumper: true, pageSize: 5, total: 50}"
+    >
+      <a-list-item slot="renderItem" slot-scope="item">
+       
+        <template>
+          <a-card :hoverable="true">
+            <a-card-meta >
+              <div style="margin-bottom: 3px" slot="title">{{item.title}}</div>
+              <a-avatar class="card-avatar" slot="avatar" :src="item.avatar" size="large" />
+              <div class="meta-content" slot="description">{{item.content}}</div>
+            </a-card-meta>
+            <a slot="actions">详情</a>
+            <a slot="actions">删除</a>
+          </a-card>
+        </template>
+      </a-list-item>
+    </a-list>
+  </div>
+  </div>
+
+  
+</template>
+
+<script>
+import PageHeader from '@/components/page/header/PageHeader'
+const dataSource = []
+dataSource.push({
+  add: true
+})
+for (let i = 0; i < 11; i++) {
+  dataSource.push({
+    title: 'Alipay',
+    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png',
+    content: '在中台产品的研发过程中，会出现不同的设计规范和实现方式，但其中往往存在很多类似的页面和组件，这些类似的组件会被抽离成一套标准规范。'
+  })
+}
+
+export default {
+  name: 'CardList',
+  components: {PageHeader},
+  
+  data () {
+    return {
+      desc: '数据集是 OCR 模型训练和评估的基础，支持多种类型的文档图像数据集管理，包括表格、发票、合同等。您可以创建、导入、编辑和管理不同类型的数据集，用于模型训练和性能评估。',
+      linkList: [
+        {icon: 'plus-circle', href: '/#/', title: '创建数据集'},
+        {icon: 'upload', href: '/#/', title: '导入数据'},
+        {icon: 'file-text', href: '/#/', title: '使用说明'}
+      ],
+      extraImage: 'https://gw.alipayobjects.com/zos/rmsportal/RzwpdLnhmvDJToTdfDPe.png',
+      dataSource
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+ .card-list{
+    margin-top: 24px;
+  }
+ .page-header{
+    margin: 0 -24px 0;
+  }
+  .link{
+    /*margin-top: 16px;*/
+    line-height: 24px;
+    a{
+      font-size: 14px;
+      margin-right: 32px;
+      i{
+        font-size: 22px;
+        margin-right: 8px;
+      }
+    }
+  }
+  .page-content{
+    position: relative;
+    padding: 24px 0 0;
+    &.side{
+    }
+    &.head.fixed{
+      margin: 0 auto;
+      max-width: 1400px;
+    }
+  }
+
+  .card-avatar {
+    width: 48px;
+    height: 48px;
+    border-radius: 48px;
+  }
+  .new-btn{
+    border-radius: 2px;
+    width: 100%;
+    height: 187px;
+  }
+  .meta-content{
+    position: relative;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    height: 64px;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+
+</style>
