7dc5be99c0323e0a8b084ff8730e5ddb87aca6f1
techflag <562635045@qq.com>
2025-05-13 19:12:15 +0800

    更新配置

--- server/config/mysqlConfig.py
+++ server/config/mysqlConfig.py
@@ -1,6 +1,6 @@
 class MysqlConfig:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:5f8e6e1540a93ddf@47.99.148.195:3306/ocr_recovery'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://your_username:your_ip:3306/ocr_recovery'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_ENGINE_OPTIONS = {'connect_args': {'init_command': "SET time_zone='+08:00';"}}
